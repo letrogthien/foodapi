49a2f003633a50a68f9b5e56d96ddeab758f4909
le trong thien <letrogthien@gmail.com>
2024-04-06 00:43:06 +0700

    first

---
+++ src/test/java/com/foodapi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
