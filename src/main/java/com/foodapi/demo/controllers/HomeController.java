4250347b474882a2c9b7755a427e2e398630af59
le trong thien <letrogthien@gmail.com>
2024-04-28 13:54:53 +0700

    ok

---
+++ src/main/java/com/foodapi/demo/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.foodapi.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("")
+    public String getMethodName() {
+        return "index";
+    }
+    
+}
